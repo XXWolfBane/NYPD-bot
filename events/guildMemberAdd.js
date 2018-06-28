09a0cb47fd85983185de16a79cd643298623908a
XXWolfBane <XXWolfBane@Gmail.com>
2018-06-28 19:27:54 -0400

    Create guildMemberAdd.js

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,16 @@
+const {baselogger} = require('../logger.js')
+
+module.exports = (bot, member) => {
+  let guild = member.guild
+  let channnel = guild.channels.find('name', 'welcome')
+  
+  if (!channel) {
+    return;
+  } else {
+    if (member.displayName) {
+      channel.send(`Welcome to ${guild.name}, ${member.displayName}!`)
+    } else {
+      channel.send(`Welcome to ${guild.name}, ${member.user.username}!`)
+    }
+  }
+}
